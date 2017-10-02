ec9cb49ed87c9e58e910b0eaf48a2366159c0e92
BlackEagle <ike.devolder@gmail.com>
2017-10-02 14:22:56 +0200

    get the internal resolver from the container
    Don't just assume docker will have 127.0.0.11 available, tools like
    dinghy use other ns address for internal resolving
    
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- stable/scripts/entrypoint.sh
+++ stable/scripts/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
-if [ ! -z $DNS_RESOLVER ]; then
-    sed -e "s/\(resolver\).*/\1 $DNS_RESOLVER;/" \
-        -i /etc/nginx/include/pimcore.conf
+if [ -z $DNS_RESOLVER ]; then
+    DNS_RESOLVER=$(grep -m1 nameserver /etc/resolv.conf \
+        | sed -e 's/nameserver\s//g')
+
 fi
+sed -e "s/\(resolver\).*/\1 $DNS_RESOLVER;/" \
+    -i /etc/nginx/include/pimcore.conf
 
 if [ ! -z $PIMCORE_FRONTEND_MODULE ]; then
     sed -e "s/\(set \$pimcore_frontend_module\).*/\1 $PIMCORE_FRONTEND_MODULE;/" \
